31c65e6a5901d4121399e6bf721e5d41e0390e79
Max Marshall <sorrybutthisusernameistaken@gmail.com>
2016-06-04 14:08:57 +0200

    Added a 'Time shift stone'.

---
+++ Assets/Scripts/TimeShiftStone/Dragger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.TimeShiftStone
+{
+    public class Dragger : MonoBehaviour
+    {
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            bool mouse = Input.GetMouseButton(0);
+            if (mouse && Input.mousePosition.y < Screen.height - 60)
+            {
+                Plane plane = new Plane(new Vector3(0, 0, 1), new Vector3(0, 0, 0));
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                float d;
+                plane.Raycast(ray, out d);
+                //if(Input.GetMouseButtonDown(0))
+                //	rigidbody.position = ray.GetPoint(d);
+                //else
+                GetComponent<Rigidbody>().velocity = -(GetComponent<Rigidbody>().position - ray.GetPoint(d)) / Time.deltaTime / 10f;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/TimeShiftStone/GroundTexturizer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.Scripts.TimeShiftStone
+{
+    [ExecuteInEditMode]
+    public class GroundTexturizer : MonoBehaviour
+    {
+
+        public GameObject[] objectsToUpdate;
+        public float radius = 5;
+        public float borderWidth = 0.1f;
+        public Color borderColor;
+        public bool cylindrical = false;
+
+        // Update is called once per frame
+        void Update()
+        {
+            foreach (GameObject o in objectsToUpdate)
+            {
+                o.GetComponent<Renderer>().sharedMaterial.SetVector("_LightPos", transform.position);
+                o.GetComponent<Renderer>().sharedMaterial.SetFloat("_LightRad", radius);
+                o.GetComponent<Renderer>().sharedMaterial.SetColor("_BorderColor", borderColor);
+                o.GetComponent<Renderer>().sharedMaterial.SetFloat("_BorderWidth", borderWidth);
+                o.GetComponent<Renderer>().sharedMaterial.SetFloat("_Cylindrical", cylindrical ? 1 : 0);
+            }
+        }
+
+        void OnGUI()
+        {
+            radius = GUI.HorizontalSlider(new Rect(10, 10, 200, 40), radius, 0, 10);
+        }
+    }
+}
